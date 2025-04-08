8808d9acd141f39a315c37b752dc62800e07bbab
Metra2015 <artem.yushak@gmail.com>
2025-04-08 15:07:17 -0400

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,10 @@
 import React from 'react';
-function App() { return <div>Welcome to SentinelX</div>; }
+function App() {
+  return (
+    <div className="App">
+      <h1>SentinelX is ready!</h1>
+      <p>Frontend is now working. Time to wire it to the backend.</p>
+    </div>
+  );
+}
 export default App;
